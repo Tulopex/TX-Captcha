2b2ee6bcbbacabbedbd02ca7248820d64a9e81ff
Tulopex <andoskinvadim2022@gmail.com>
2024-02-07 14:37:40 +0300

    add js and css

---
+++ TX-Captcha/index.js
@@ -0,0 +1,18 @@
+let isChecked = false;
+    
+document.getElementById("captchaSquare").onclick = function() {
+    isChecked = !isChecked;
+    this.classList.toggle("checked");
+};
+
+function checkCaptcha() {
+    if (isChecked) {
+        document.getElementById("txCaptcha").style.display = "none";
+        document.getElementById("mainContent").style.display = "block";
+    } else {
+        document.getElementById("captchaMessage").innerText = "TX Captcha не пройдена";
+    }
+}
+window.onload = function() {
+    document.getElementById("captchaContainer").style.display = "block";
+};
